3f9772e7f5a09e31b87f8d822b3497b4be47597a
konst <konstantin.mikheev@icord.cz>
2021-05-22 23:11:43 +0200

    model and removed unused types

--- src/core/custom-types/react-fela.d.ts
+++ src/core/custom-types/react-fela.d.ts
@@ -2,14 +2,4 @@ import * as f from 'react-fela';
 
 declare module 'react-fela' {
   export type FelaRule<P = {}> = FelaStyle<{}, P>;
-
-  export type CSSThemeFunction<T = {}, P> = (...style: FelaStyle<T, P>[]) => string;
-
-  export interface FelaHookProps<T, P> {
-    css: CSSThemeFunction<T, P>;
-    theme: {};
-    renderer: IRenderer;
-  }
-
-  export function useFela<T = {}, P = {}>(props?: P): FelaHookProps<T, P>;
 }
--- src/core/models/LocketModel.ts
+++ src/core/models/LocketModel.ts
@@ -1,6 +1,8 @@
 import { MobileOS, MobileProcessor } from './enums';
 
 export type MobileDisplay = '6,5' | '6,1' | '6,7' | '4,7';
+export type MobileRam = 16 | 12 | 8 | 6;
+export type MobileInternalMemory = 512 | 256 | 128 | 64 | 32;
 
 export type LocketItem = {
   id: number;
@@ -15,4 +17,24 @@ export type LocketParameters = {
   os: MobileOS;
   processor: MobileProcessor;
   display: MobileDisplay;
+};
+
+export type LocketDetail = {
+  parameters: {
+    ram: MobileRam;
+    internalMemory: MobileInternalMemory;
+  } & LocketParameters;
+  shops: ShopInfo[];
+  reviews: ReviewInfo[];
+} & Omit<LocketItem, 'parameters'>;
+
+export type ShopInfo = {
+  name: string;
+  price: string;
+  link: string;
+};
+export type ReviewInfo = {
+  name: string;
+  feedback: string;
+  positive: boolean | null;
 };
