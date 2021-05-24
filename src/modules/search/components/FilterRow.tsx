d5c15e50a1f24cdcd46b51b7e1b1b13984d4f5ef
konst <konstantin.mikheev@icord.cz>
2021-05-24 09:03:14 +0200

    filter row props test

--- src/modules/search/components/FilterRow.tsx
+++ src/modules/search/components/FilterRow.tsx
@@ -17,7 +17,12 @@ export function FilterRow<T>({ title, options, onChange }: Props<T>) {
     <div className={css({ display: 'flex', flexDirection: 'column', margin: '.25rem .5rem 0 1rem', cursor: 'pointer' })}>
       <b className={css({ marginBottom: '.5rem' })}>{title}</b>
       {options.map(option => (
-        <span key={`${option.value}`} className={css({ marginBottom: '.5rem' })} onClick={() => onChange(option.value)}>
+        <span
+          key={`${option.value}`}
+          className={css({ marginBottom: '.5rem' })}
+          onClick={() => onChange(option.value)}
+          data-testid={`${option.value}`}
+        >
           <NativeCheckBox onChange={() => onChange(option.value)} checked={option.checked} computedId={`${option.value}`} />
           {option.label}
         </span>
--- src/modules/search/test/FilterRow.test.tsx
+++ src/modules/search/test/FilterRow.test.tsx
@@ -1,17 +1,19 @@
+import { fireEvent, render } from '@testing-library/react';
 import { noop } from 'core/data/utils';
 import { FelaProvider } from 'core/fela/Provider';
 import renderer from 'react-test-renderer';
+import { spy } from 'sinon';
 import { FilterRow } from '../components/FilterRow';
 
-const options = [
+const testOptions = [
   {
     label: 'test option 1',
-    value: 'test value 1',
+    value: 'test_value_1',
     checked: false
   },
   {
     label: 'test option 2',
-    value: 'test value 2',
+    value: 'test_value_2',
     checked: true
   }
 ];
@@ -19,9 +21,27 @@ const options = [
 test('FilterRow renders correctly', () => {
   const component = renderer.create(
     <FelaProvider>
-      <FilterRow<string> title="Test filter" options={options} onChange={noop} />
+      <FilterRow<string> title="Test filter" options={testOptions} onChange={noop} />
     </FelaProvider>
   );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+describe('<FilterRow />', () => {
+  it('should have all required props: title, options, onChange', () => {
+    const handleChange = spy();
+    const { queryByText, getByTestId } = render(
+      <FelaProvider>
+        <FilterRow<string> title="Test Filter" options={testOptions} onChange={handleChange} />
+      </FelaProvider>
+    );
+
+    expect(queryByText('Test Filter')).toBeTruthy();
+    expect(queryByText(testOptions[0].label)).toBeTruthy();
+    expect(queryByText(testOptions[1].label)).toBeTruthy();
+
+    fireEvent.click(getByTestId(testOptions[0].value));
+    expect(handleChange.callCount).toEqual(1);
+  });
 });
